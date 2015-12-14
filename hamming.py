7c953295dfea5c3efccfb598a15fc72edcbad8da
Mark Wyse <wysem@cs.washington.edu>
2015-12-14 15:24:46 -0800

    hamming experiment file

---
+++ hamming.py
@@ -0,0 +1,64 @@
+import sys
+import time
+import random
+from algorithms import Alignment
+from pyDatalog import pyDatalog
+
+# create some python functions as helpers, because pyDatalog allows this
+
+# string length function
+def strlen(x):
+    if isinstance(x,basestring):
+        return len(x)
+    return 0
+
+# create required terms
+# variables
+pyDatalog.create_terms('X,Y,Z,LX,LY,N')
+
+def score(a,b):
+    if a==b:
+        return 0
+    return 1
+
+pyDatalog.create_terms('hamming,words,w,score,hammingscores,S,HS,K,hs,strlen')
+
+#w = ['hello','helps','world','worne']
+def build_hamming(args):
+    w = args
+    for ww in w:
+        + words(ww)
+
+    hammingscores(X,Y,K,S) <= words(X) & words(Y) & (K.in_(range_(strlen(X)))) & (strlen(X)==strlen(Y)) & (S==score(X[K],Y[K]))
+    #print(hammingscores(X,Y,N,S))
+    
+    # hamming distance function
+    (hs[X,Y]==sum_(S, for_each=N)) <= hammingscores(X,Y,N,S)
+
+    # query for a specific X,Y pair of words within hammingscores table
+    #print(hs['hello','world']==Y)
+
+    # generate hamming distance for every X,Y pair in hammingscores table
+    #hs(X,Y,HS) <= hammingscores(X,Y,N,S) & (HS==hs[X,Y])
+    #print(hs(X,Y,HS))
+
+def ask(a,b):
+    print(hs[a,b]==S)
+
+def exp(l):
+    alphabet = ['A','T','C','G']
+    a = ''.join([random.choice(alphabet) for i in xrange(l)])
+    b = ''.join([random.choice(alphabet) for i in xrange(l)])
+    t = time.time()
+    build_hamming([a,b])
+    ask(a,b)
+    print('Time to build and ask hamming: {0}'.format(time.time() - t))
+
+def usage():
+    print('python hamming.py <sequence length>')
+    sys.exit(-1)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+    exp(int(sys.argv[1]))
