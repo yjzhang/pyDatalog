5bf89b49c584eaa3395fcd75722b3311ec3af776
Mark Wyse <wysem@cs.washington.edu>
2015-12-14 15:56:57 -0800

    hamming and exact match experiment scripts

--- hamming.py
+++ hamming.py
@@ -24,7 +24,7 @@ def score(a,b):
 pyDatalog.create_terms('hamming,words,w,score,hammingscores,S,HS,K,hs,strlen')
 
 #w = ['hello','helps','world','worne']
-def build_hamming(args):
+def build(args):
     w = args
     for ww in w:
         + words(ww)
@@ -33,7 +33,7 @@ def build_hamming(args):
     #print(hammingscores(X,Y,N,S))
     
     # hamming distance function
-    (hs[X,Y]==sum_(S, for_each=N)) <= hammingscores(X,Y,N,S)
+    #(hs[X,Y]==sum_(S, for_each=N)) <= hammingscores(X,Y,N,S)
 
     # query for a specific X,Y pair of words within hammingscores table
     #print(hs['hello','world']==Y)
@@ -43,16 +43,30 @@ def build_hamming(args):
     #print(hs(X,Y,HS))
 
 def ask(a,b):
+    # hamming distance function
+    (hs[X,Y]==sum_(S, for_each=N)) <= hammingscores(X,Y,N,S)
     print(hs[a,b]==S)
 
 def exp(l):
     alphabet = ['A','T','C','G']
     a = ''.join([random.choice(alphabet) for i in xrange(l)])
     b = ''.join([random.choice(alphabet) for i in xrange(l)])
+
+    # perform experiment in pyDatalog
     t = time.time()
-    build_hamming([a,b])
+    build([a,b])
     ask(a,b)
-    print('Time to build and ask hamming: {0}'.format(time.time() - t))
+    pyDatalogTime = time.time() - t
+    print('Time in pyDatalog: {0}'.format(pyDatalogTime))
+
+    # perform experiment in native python
+    t = time.time()
+    res = Alignment.Hamming(a,b)
+    print(res)
+    pyTime = time.time() - t
+    print('Time in native python: {0}'.format(pyTime))
+
+    return pyDatalogTime, pyTime
 
 def usage():
     print('python hamming.py <sequence length>')
